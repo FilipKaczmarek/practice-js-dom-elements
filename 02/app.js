f062cad67fd70491935c7a7dda9ce643a9f38721
Filip <filipkaczmarek@macbook-air.home>
2021-03-24 18:55:05 +0100

    subtask 3

--- 02/app.js
+++ 02/app.js
@@ -1,15 +1,18 @@
 console.log('DOM');
 
+// wyszukanie wszystkich linków, które posiadają atrybut data-url.
 const linksWithAttribute = document.querySelectorAll('a[data-url]');
 const convertedArray = Array.from(linksWithAttribute)
-console.log(convertedArray[2].getAttribute('data-url'))
 
-// convertedArray.reduce( (acc, item) => {
-//     acc[item] = 'aa'
-//     return acc
-// }, {})
-
+// Następnie należy pobrać zawartość tego atrybutu do zmiennej.
+// Potem wartość tej zmiennej przypisać do atrybutu href wyszukanego elementu </a>.
 const newArr = convertedArray.map( (el, index) => {
-   return el[index] = el.getAttribute('data-url')
+   return el[index] = el.getAttribute('data-url');
 })
-console.log(newArr)
+
+const [ devMentor, developerMozilla, stackOverFlow ] = newArr;
+
+convertedArray[0].href = devMentor;
+convertedArray[1].href = developerMozilla;
+convertedArray[2].href = stackOverFlow;
+
