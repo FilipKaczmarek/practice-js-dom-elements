e03d0d787345644522553c202e3e84f5557e884d
Filip <filipkaczmarek@macbook-air.home>
2021-03-24 18:09:07 +0100

    exercise 1 / subtask 2

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,24 @@
 console.log('DOM');
+
+// Wyszukaj element, który posiada dwie klasy tj. `.comments__item` oraz `.comments__item--newest`.
+const comments = document.querySelector('.comments');
+let containsTwoClasses = null;
+
+function findIfContains(){
+for (let i = 0; i < comments.childNodes.length; i++){
+    if(comments.childNodes[i].classList == "comments__item comments__item--newest"){
+        const className = comments.childNodes[i]
+        return className;
+    }
+}
+return className;
+}
+
+containsTwoClasses = findIfContains();
+
+// Następnie przeszukaj ten element względem znaczników, które posiadają atrybut `data-info`.
+
+const elementWithTwoClasses = document.querySelector('.comments__item.comments__item--newest');
+console.log(elementWithTwoClasses);
+
+
