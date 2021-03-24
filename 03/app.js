04ea16c567230c47a4f70a9e10327780c5c55c8d
Filip <filipkaczmarek@macbook-air.home>
2021-03-24 19:52:14 +0100

    Task 3 | Method 2

--- 03/app.js
+++ 03/app.js
@@ -16,10 +16,23 @@ const buttonSettings = {
 // My code: 
 const sectionForButton = document.querySelector('.parent-for-button');
 
-// First option (with template)
+// First method (with template)
 const template = `
     <button class="${buttonSettings.attr?.className}" title="${buttonSettings.attr?.title}">${buttonSettings.text}</button>
 `;
 sectionForButton.innerHTML += template;
 
-// Second option (without template but with for in loop)
+// Second method (without template)
+const button2 = document.createElement('button');
+button2.classList.add(`${buttonSettings.attr.className}`);
+button2.setAttribute('title' , buttonSettings.attr.title);
+
+button2.style.border = `${buttonSettings.css.border}`;
+button2.style.padding = `${buttonSettings.css.padding}`;
+button2.style.color = `${buttonSettings.css.color}`;
+
+button2.innerHTML += `${buttonSettings.text}`;
+
+sectionForButton.appendChild(button2);
+
+// Third method (for in loop)
