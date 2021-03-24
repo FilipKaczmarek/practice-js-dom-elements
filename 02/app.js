408f4d7e8adf4bed19ef10f3e4b187b7035fa8ba
Filip <filipkaczmarek@macbook-air.home>
2021-03-24 18:37:00 +0100

    Task 2 - subtask 1 & 2

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,15 @@
 console.log('DOM');
+
+const linksWithAttribute = document.querySelectorAll('a[data-url]');
+const convertedArray = Array.from(linksWithAttribute)
+console.log(convertedArray[2].getAttribute('data-url'))
+
+// convertedArray.reduce( (acc, item) => {
+//     acc[item] = 'aa'
+//     return acc
+// }, {})
+
+const newArr = convertedArray.map( (el, index) => {
+   return el[index] = el.getAttribute('data-url')
+})
+console.log(newArr)
